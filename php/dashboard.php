bf689a41bc99e351aab4c0513909a8118262fa6d
jssk20 <jaisurya20kct@gmail.com>
2025-11-12 17:24:56 +0530

    php removed

--- php/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION["user_id"])) {
-    header("Location: ../topics/login.html");
-    exit;
-}
-echo "Welcome, " . htmlspecialchars($_SESSION["username"]) . "!";
-header("Location: ../topics/home.php");
-?>
--- php/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Remove all session variables
-session_destroy(); // Destroy the session
-header("Location: ../topics/home.php"); // Redirect to homepage or login
-exit();
-?>
